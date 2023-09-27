ab80c4dfb75b3892393699289ff8257d256add22
Matheus Sabino Souza <matheus.souza@ctw.bmwgroup.com>
2023-09-27 20:49:12 +0100

    Test local image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
           >
             By{' '}
             <Image
-              src="/vercel.svg"
+              src="https://sabrinovsky.github.io/me/eu.jpg"
               alt="Vercel Logo"
               className="dark:invert"
               width={100}
